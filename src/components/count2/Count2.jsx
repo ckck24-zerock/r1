be5df7c18872fba848d32ab58ef7ce8c126c1c6c
zerock <kangckck24@gmail.com>
2025-02-26 13:44:10 +0900

    fff

--- src/components/count2/Count2.jsx
+++ src/components/count2/Count2.jsx
@@ -2,15 +2,17 @@ import {useState} from "react";
 
 function Count2() {
 
+    //기존의 보관하고 상태객체와 다른 객체일 경우에만 다시 렌더링 됩니다.
     //number였을때는 문제가 없었다.
     const [count, setCount] = useState({num:0})
 
     const plus = () => {
-        count.num += 1
 
-        console.log(count)
+        //const temp = {...count} //새로운 메모리공간을 만들어서 count객체를 쫘악 펴서 복사
+        //temp.num += 1
+        //setCount(temp)
 
-        setCount({...count})
+        setCount({num: count.num + 1})
     }
 
 
