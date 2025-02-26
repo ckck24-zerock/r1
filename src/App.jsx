a328c79992a582f7d0817a3eaf7fd7b07507b3ca
zerock <kangckck24@gmail.com>
2025-02-26 14:05:04 +0900

    fff

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import TodoList from "./components/todo/todoList.jsx";
 import ForEx from "./components/count/forEx.jsx";
 import ForEx2 from "./components/count/forEx2.jsx";
 import Count2 from "./components/count2/Count2.jsx";
+import ArrayTest from "./components/count2/ArrayTest.jsx";
 
 function App() {
 
   return (
     <div className={'m-2 p-2 bg-blue-200 border-2'}>
         <Count2></Count2>
+        <ArrayTest></ArrayTest>
     </div>
   )
 }
---
+++ src/components/count2/ArrayTest.jsx
@@ -0,0 +1,24 @@
+
+
+function ArrayTest() {
+
+
+    return (
+        <div>
+            <div className={'border-2'}>
+                <button className="px-6 py-3 bg-blue-500 text-white font-semibold rounded-xl shadow-md transition
+                 hover:bg-blue-600 active:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400
+                 dark:bg-blue-700 dark:hover:bg-blue-800 dark:focus:ring-blue-500">CLICK</button>
+            </div>
+
+            <div className={'bg-amber-300'}>
+                <ul>
+
+                </ul>
+            </div>
+
+        </div>
+    );
+}
+
+export default ArrayTest;
