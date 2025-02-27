aefd2feaa5a943ac20f1bea6bf84b81825aceead
zerock <kangckck24@gmail.com>
2025-02-27 09:40:30 +0900

    fff

--- src/components/todo2/todoInput.jsx
+++ src/components/todo2/todoInput.jsx
@@ -1,11 +1,18 @@
+import {useState} from "react";
 
 function TodoInput() {
+
+    const [text, setText] = useState('ABC')
+
     return (
         <div className={'p-3 bg-amber-300 h-1/2'}>
             <div>Todo Input</div>
             <div className={'p-2 font-bold m-2 p-2'}>
-                <input type={'text'} className={'bg-white p-2 w-2/3'}/>
-                <button>SAVE</button>
+                <input type={'text'}
+                       className={'bg-white p-2 w-2/3'}
+                       value={text}
+                />
+                <button className={'border-1 ml-3 p-3 bg-blue-300'}>SAVE</button>
             </div>
         </div>
     );
