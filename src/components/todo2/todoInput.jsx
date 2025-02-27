b51aa914ca882139c02ff35d651b4a355b1306b7
zerock <kangckck24@gmail.com>
2025-02-27 09:36:48 +0900

    fff

--- src/components/todo2/todoInput.jsx
+++ src/components/todo2/todoInput.jsx
@@ -3,6 +3,10 @@ function TodoInput() {
     return (
         <div className={'p-3 bg-amber-300 h-1/2'}>
             <div>Todo Input</div>
+            <div className={'p-2 font-bold m-2 p-2'}>
+                <input type={'text'} className={'bg-white p-2 w-2/3'}/>
+                <button>SAVE</button>
+            </div>
         </div>
     );
 }
